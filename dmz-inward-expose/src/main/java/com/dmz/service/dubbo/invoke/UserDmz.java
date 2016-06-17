aecec44b2f17ba41748ace30cc526af2a1671991
dreamming <527386108@qq.com>
2016-06-17 12:13:45 +0800

    update something

--- dmz-inward-expose/src/main/java/com/dmz/service/dubbo/invoke/UserDmz.java
+++ dmz-inward-expose/src/main/java/com/dmz/service/dubbo/invoke/UserDmz.java
@@ -4,5 +4,10 @@ package com.dmz.service.dubbo.invoke;
  * Created by dmz on 2016/3/5.
  */
 public interface UserDmz {
+    /**
+     * 展示用户名称
+     *
+     * @return
+     */
     public String showUserName();
 }
